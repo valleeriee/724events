4363cf5afbf899266077a435ea1aec737c4e710a
Valérie <valleeriee@yahoo.fr>
2024-10-30 22:23:43 +0100

    Affichage du contenu de l'event "Notre derniére prestation" dans le footer

--- src/contexts/DataContext/index.js
+++ src/contexts/DataContext/index.js
@@ -19,6 +19,7 @@ export const api = {
 export const DataProvider = ({ children }) => {
   const [error, setError] = useState(null);
   const [data, setData] = useState(null);
+  const [last, setLast] = useState(null);
   const getData = useCallback(async () => {
     try {
       setData(await api.loadData());
@@ -27,7 +28,11 @@ export const DataProvider = ({ children }) => {
     }
   }, []);
   useEffect(() => {
-    if (data) return;
+    if (data) {
+      const lastIndex = data.events.length;
+      setLast(data.events[lastIndex - 1]);
+      return;
+    }
     getData();
   });
   
@@ -37,6 +42,7 @@ export const DataProvider = ({ children }) => {
       value={{
         data,
         error,
+        last,
       }}
     >
       {children}
